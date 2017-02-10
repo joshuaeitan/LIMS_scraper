b021d42368abfa0e750648d8a75d4192a6c71a8d
Josh Kaplan <joshua.eitan.kaplan@gmail.com>
2017-02-10 16:17:44 -0500

    tesseract OCR working

--- LIMS_scraper/LIMS_scraper_util.py
+++ LIMS_scraper/LIMS_scraper_util.py
@@ -1,9 +1,19 @@
 import requests
 import re
 import json 
-from subprocess import PIPE, Popen
+from subprocess import PIPE, Popen, check_call
 from unipath import Path
 import os.path
+import pytesseract
+from PIL import Image
+import glob
+
+def convertToRegex(searchTerm):
+    words = searchTerm.split(' ')
+    regExTerm = '(?i)'
+    for word in words:
+        regExTerm +=word+'\s*'
+    return re.compile(regExTerm)
 
 def downloadToText(r,path):
     ## To download files, convert PDF to text
@@ -17,11 +27,26 @@ def downloadToText(r,path):
         loc.append(path+ids[i][1]+'-SignedAct.pdf')
         if not os.path.isfile(loc[i]):
             response = requests.get(url)
-            ## can we do OCR here without acually downloading (writing to hard drive) the pdfs? - it looks like we cant, extraction tools dont recognize bytes object (response.content) as a PDF
-            with open(loc[i], 'wb') as f:
-                f.write(response.content)
-            Popen(['pdftotext', '-enc', 'UTF-8','-layout' , loc[i]], stdout=PIPE)
-    return loc
+            #if the file doesnt exist, the downloaded 'pdf' will be 531 bytes
+            if len(response.content)!=531:
+                with open(loc[i], 'wb') as f:
+                    f.write(response.content)
+                    # if file is already OCR'd, just use pdftotext to pull text
+                check_call(['pdftotext', '-enc', 'UTF-8','-layout' , loc[i]], stdout=PIPE)
+                with open(loc[i][:-3]+'txt','r') as f:
+                    # empty files (length 3 when read) are created when we call pdftotext on a non-OCR'd pdf
+                    if len(f.read())<10:
+                        # if file is not OCR'd (older PDFs and some angled scans), convert PDFs to png (smaller and higher qual than tiff and run tesseract OCR
+                        params = ['convert', '-density','300', '-units','PixelsPerInch', '-type','Grayscale', loc[i], loc[i][:-3]+'png']
+                        check_call(params)
+                        # converting to png outputs one file for each page, with -1,-2,...-n.png extensions - need a way to determine how many files are created
+                        pages = glob.glob(loc[i][:-4]+'-*')
+                        text = ''
+                        for page in pages:
+                            text+=pytesseract.image_to_string(Image.open(page))
+                        with open(loc[i][:-3]+'txt','w') as f:
+                            f.write(text)
+    return ids,loc
 
 def search(loc,searchTerm):
     parSplit = re.compile('\n\n')
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from LIMS_scraper.LIMS_scraper_util import * 
-import requests
-from unipath import Path
-import re
 
+# need to convert older pdfs (without OCR) and newer angled scans to some kind of image format
+# try pdftotext first - if we can just pull text, no need to convert and run OCR
+# need to check if pdftotext file is empty
+
+'convert -density 300 -units PixelsPerInch -type Grayscale +compress input.png input.tif'
 #end goal: user inputs LIMS advanced search criteria for files to download and term to pull paragraphs based on - if files are already downloaded, skip to search step. 
+
+searchTerm = convertToRegex(input("What would you like to search for? "))
 
 # base url
 base = 'http://lims.dccouncil.us'
@@ -29,16 +33,13 @@ path = Path('/Users/joshuakaplan/Documents/Georgetown/Spring 2017/Dr Bailey/Scra
 
 r = requests.post(base+'/api/v1/Legislation/AdvancedSearch',json=criteria)
 
-searchTerm = re.compile('(?i)Shall\s*Evaluate')
-searchTerm = re.compile('(?i)District\s*of\s*Columbia')
-
 def main(criteria,path,r,searchTerm):
     # download files, convert PDFs to text, create list of file locations
-    loc = downloadToText(r,path)
+    ids, loc = downloadToText(r,path)
     # search downloaded and converted files, pull paragraphs containing search term
     results = search(loc,searchTerm)
     # save search results to file
     saveSearchResults(results,path)
 
 if __name__ == '__main__':
-    main(criteria,path,r,searchTerm)
+    main(criteria,path,r,searchTerm)  
